b672a0d620874b8046da5022c4c611fd1a9889cf
Samuel Sekiwere <sekiskylink@gmail.com>
2012-11-24 15:54:17 +0300

    fix

--- dhis2/utils.py
+++ dhis2/utils.py
@@ -38,7 +38,9 @@ dbuser = db_config['USER']
 dbpasswd = db_config['PASSWORD']
 dbport = db_config['PORT']
 
-conn = psycopg2.connect("dbname=" + dbname + " host= " + dbhost + " user=" + dbuser + " password=" + dbpasswd + " port= %s" % dbport)
+port_param = " port = %s" % dbport
+
+conn = psycopg2.connect("dbname=" + dbname + " host= " + dbhost + " user=" + dbuser + " password=" + dbpasswd + port_param)
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 http = Http()
