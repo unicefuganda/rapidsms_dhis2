3c47dc4d1d3fdddf283e3dc2c9b1b690831fff4c
NavaL Andrian <immotep@gmail.com>
2013-03-14 15:51:58 +0300

    +Nishant: changed submission task

--- dhis2/reports_submission_tasks.py
+++ dhis2/reports_submission_tasks.py
@@ -1,9 +1,16 @@
 from dhis2.h033b_reporter import *
 from celery.task.schedules import crontab  
 from celery.decorators import periodic_task  
+from celery import Celery, current_task
+
+celery = Celery()
   
-# this will at midnight on every Thursday of the week, see http://celeryproject.org/docs/reference/celery.task.schedules.html#celery.task.schedules.crontab  
-@periodic_task(run_every=crontab(hour="23", minute="59", day_of_week="thursday"))  
+# this will at midnight on every Thursday of the week, see http://celeryproject.org/docs/reference/celery.task.schedules.html#celery.task.schedules.crontab 
+@celery.task
 def weekly_report_submissions_task(): 
   h033b_reporter = H033B_Reporter()
   h033b_reporter.initiate_weekly_submissions()
+
+
+  
+  
