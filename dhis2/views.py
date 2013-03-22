7ccc3b92225237d977e7c4e41da0ffe83662810c
NavaL <mnandri@thoughtworks.com>
2013-03-22 13:56:31 +0300

    changed dhis2reporter index page

--- dhis2/views.py
+++ dhis2/views.py
@@ -9,9 +9,18 @@ from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from dhis2.models import Dhis2_Reports_Report_Task_Log,Dhis2_Reports_Submissions_Log
 import datetime
+import mock
 
 TASK_LOG_RECORDS_PER_PAGE = 10
 TASK_SUBMISSIONS_LOG_RECORDS_PER_PAGE = 10
+
+RESULT_URLS = {
+    Dhis2_Reports_Submissions_Log.SUCCESS                 : [' Successful submissions', ''], # we provide no urls for success -- too many and no need for them
+    Dhis2_Reports_Submissions_Log.FAILED                  : [' Network failure or other failed submissions','failed'],
+    Dhis2_Reports_Submissions_Log.ERROR                   : [' With invalid orgUnit or diseases ID changed','errors'],
+    Dhis2_Reports_Submissions_Log.INVALID_SUBMISSION_DATA : [' No HMIS003b indicators',''], # invalid means, no HMIS indicators, no need to display them.
+    Dhis2_Reports_Submissions_Log.SOME_ATTRIBUTES_IGNORED : [' Ignored', 'ignored'],
+}
 
 def index(request):
   task_logs = _get_tasks_view_data()
@@ -30,48 +39,28 @@ def index(request):
   
   return render(request, 'h033b_reporter_index.html', {'tasks_logs_paginator':task_logs_paginator})
 
-def _get_tasks_view_data():
-  tasks = Dhis2_Reports_Report_Task_Log.objects.all()
-  data = []
-  for task in tasks : 
-    task_view_data = task
-    task.time_finished = task.time_finished if task.time_finished else datetime.datetime.now() 
-    running_time_in_minutes = (task.time_finished - task.time_started).seconds/60 
-    task_view_data.running_time = str(running_time_in_minutes)+' minutes'     
-    data.append(task_view_data)
-  
-  return data  
-
 def task_failed(request,task_id):
-  return _generate_log_page(request=request, task_id=task_id, result = Dhis2_Reports_Submissions_Log.FAILED, view_html='errors.html')
+  return _generate_log_page(request=request, task_id=task_id, result = Dhis2_Reports_Submissions_Log.FAILED, view_html='log_details.html')
 
 def task_errors(request,task_id):
-  return _generate_log_page(request=request, task_id=task_id, result = Dhis2_Reports_Submissions_Log.ERROR, view_html='errors.html')
+  return _generate_log_page(request=request, task_id=task_id, result = Dhis2_Reports_Submissions_Log.ERROR, view_html='log_details.html')
 
 def task_ignored(request,task_id):
-  return _generate_log_page(request=request, task_id=task_id, result = Dhis2_Reports_Submissions_Log.SOME_ATTRIBUTES_IGNORED, view_html='errors.html')
+  return _generate_log_page(request=request, task_id=task_id, result = Dhis2_Reports_Submissions_Log.SOME_ATTRIBUTES_IGNORED, view_html='log_details.html')
   
 def task_summary(request,task_id):
   task = Dhis2_Reports_Report_Task_Log.objects.get(id=task_id)
-  results = [
-      Dhis2_Reports_Submissions_Log.SUCCESS,
-      Dhis2_Reports_Submissions_Log.FAILED,
-      Dhis2_Reports_Submissions_Log.ERROR,
-      Dhis2_Reports_Submissions_Log.INVALID_SUBMISSION_DATA,
-      Dhis2_Reports_Submissions_Log.SOME_ATTRIBUTES_IGNORED,
-  ]
 
   all_sub=[]
 
-  for result in results:
+  for result in RESULT_URLS.keys():
     log_result = Dhis2_Reports_Submissions_Log.objects.filter(task_id=task, result=result)
     
     if log_result:
      a_result = log_result[0]
      a_result.size = len(log_result) 
+     a_result.url = RESULT_URLS[result][1] if RESULT_URLS[result][1] else None
      all_sub.append(a_result)
-    
-  print all_sub  
     
   teplate_data = {
     'task_log'  : task,
@@ -79,7 +68,35 @@ def task_summary(request,task_id):
   }
 
   return render(request, 'submission_summary.html', teplate_data)
-    
+  
+
+def _get_tasks_view_data():
+  tasks = Dhis2_Reports_Report_Task_Log.objects.all()
+  data = []
+  for task in tasks : 
+    task_view_data = task
+    task.time_finished = task.time_finished if task.time_finished else datetime.datetime.now() 
+    running_time_in_minutes = (task.time_finished - task.time_started).seconds/60 
+    task_view_data.running_time = str(running_time_in_minutes)+' minutes'  
+    task_view_data.details = _get_task_details(task)   
+    data.append(task_view_data)
+
+  return data
+  
+def _get_task_details(task):
+  task_details=[]
+
+  for result in RESULT_URLS.keys():
+    log_result = Dhis2_Reports_Submissions_Log.objects.filter(task_id=task, result=result)
+
+    if log_result:
+      a_result = {}
+      a_result['description'] = RESULT_URLS[result][0]
+      a_result['size'] = len(log_result) 
+      a_result['url'] = RESULT_URLS[result][1] if RESULT_URLS[result][1] else None
+      task_details.append(a_result)
+
+  return task_details
   
 def _generate_log_page(request, task_id, result, view_html):
   task = Dhis2_Reports_Report_Task_Log.objects.get(id=task_id)
