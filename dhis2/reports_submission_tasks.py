83575d7208eb6502b21e34f507a80ec1afc2863a
NavaL <mnandri@thoughtworks.com>
2013-04-19 18:13:43 +0300

    added default time which is now

--- dhis2/reports_submission_tasks.py
+++ dhis2/reports_submission_tasks.py
@@ -1,9 +1,10 @@
 from dhis2.h033b_reporter import *
 from celery.task import task
 import urllib2
+from datetime import datetime
 
 @task
-def weekly_report_submissions_task(date): 
+def weekly_report_submissions_task(date=datetime.now()): 
   h033b_reporter = H033B_Reporter()
   h033b_reporter.initiate_weekly_submissions(date=date)
 
